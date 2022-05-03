5ce32097adbc5574199c79b03bdbacf5e0685165
josealonso <jose.alonso.programmer@gmail.com>
2022-05-03 18:15:19 +0200

    Modified deployment script

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,30 +1,30 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const MAX_NUM_OF_WHITELISTED_ADDRESSES = 10;
+  /*
+ A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+ so whitelistContract here is a factory for instances of our Whitelist contract.
+ */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(MAX_NUM_OF_WHITELISTED_ADDRESSES);
 
-  await greeter.deployed();
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  // print the address of the deployed contract
+  console.log(
+    "Whitelist Contract Address:",
+    deployedWhitelistContract.address
+  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
